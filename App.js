9290fbc71c2821bcde11d9e468d7a1cfc4ea0b86
Pooja Gupta <pooja@customer-acquisition.co>
2025-08-05 18:19:53 +0530

    chapter 01 : inception

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * lets try to create below strucrure with manually React CDN code
+ *
+ * <div id="parent">
+ *     <div class="child1">
+ *          <h1>I am child H1</h1>
+ *      </div>
+ *      <div class="child2">
+ *          <h2>I am child H2</h2>
+ *      </div>
+ * </div>
+ *
+ * ReactElement(Object) => HTML(Browser Understands)
+ *
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("h1", {}, "I am h1 child"),
+  React.createElement("h2", {}, "I am h2 child"),
+]);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
